5bc442c4c6a0641a637ef2deb7516f3895f97209
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-07-24 02:25:57 +0300

    feat: add BottomArrowIcon component and update FileSyntaxHighlighter to use it

--- src/components/FileSyntaxHighlighter.tsx
+++ src/components/FileSyntaxHighlighter.tsx
@@ -1,5 +1,7 @@
 import type { IFile } from "../interfaces";
+import BottomArrowIcon from "./svg/Bottom";
 import FileIcon from "./svg/File";
+import FolderIcon from "./svg/Folder";
 import RightArrowIcon from "./svg/Right";
 
 interface IProps {
@@ -10,9 +12,9 @@ const FileSyntaxHighlighter = ({ fileTree }: IProps) => {
   return (
     <div className="mb-2 ml-2">
       <div className="flex items-center mb-1">
-        <RightArrowIcon />
+        {fileTree.children ? <BottomArrowIcon /> : <RightArrowIcon />}
         <span className="mr-2">
-          <FileIcon />
+          {fileTree.isFolder ? <FolderIcon /> : <FileIcon />}
         </span>
         <span>{fileTree.name}</span>
       </div>
