6aeef4e59defa60fcfce4f10640bb43f6a6da3fd
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-07-30 01:38:56 +0300

    feat: replace FileSyntaxHighlighter with RecursiveComponent in App and enhance OpenedFilesBarTap for improved file handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import FileSyntaxHighlighter from "./components/RecursiveComponent"
 import OpenedFilesBar from "./components/OpenedFilesBar"
 import { fileTree } from "./data/fileTree"
+import RecursiveComponent from "./components/RecursiveComponent"
 
 const App = () => {
 
@@ -8,7 +8,7 @@ const App = () => {
     <div className="app">
       <div className="flex h-screen">
         <div className="w-65 border-r border-[#64646473] py-3 px-2 border-b-[1px] border-[#64646473]">
-          <FileSyntaxHighlighter fileTree={fileTree} isRoot={true} />
+          <RecursiveComponent fileTree={fileTree} isRoot={true} />
         </div>
         <OpenedFilesBar />
       </div>
--- src/components/OpenedFilesBarTap.tsx
+++ src/components/OpenedFilesBarTap.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { setClickedFile } from "../app/features/fileTreeSlice";
+import { setClickedFile, setOpenedFiles } from "../app/features/fileTreeSlice";
 import { useAppDispatch, type RootState } from "../app/store";
 import type { IFile } from "../interfaces";
 import RenderFileIcon from "./RenderFileIcon";
@@ -11,23 +11,36 @@ interface IProps {
 
 const OpenedFilesBarTap = ({ file }: IProps) => {
   const dispatch = useAppDispatch();
-  const {activeTabId} = useSelector((state: RootState) => state.fileTree.clickedFile);
+  const {openedFiles, clickedFile} = useSelector((state: RootState) => state.fileTree);
 
   const onClickedFile = () => {
     dispatch(setClickedFile({ fileName: file.name, fileContent: file.content, activeTabId: file.id }));
   }
 
+  const onRmove = (selectedId: string) => {
+    const filtered = openedFiles.filter((file) => file.id != selectedId)
+    const {id, name, content} = filtered[filtered.length - 1]
+
+    dispatch(setOpenedFiles([...filtered]))
+    dispatch(setClickedFile({fileName: name, fileContent: content, activeTabId: id}))
+  }
+
   return (
     <div key={file.id}
       className={`flex items-center border px-1 border-t-2 border-b-0
-                ${activeTabId === file.id ? "border-t-[#cf6ccf]" : "border-t-transparent"}
+                ${clickedFile.activeTabId === file.id ? "border-t-[#cf6ccf]" : "border-t-transparent"}
                 hover:bg-[#6464643b] duration-300 border-[#2a2a2a]`}
       onClick={onClickedFile}>
       <RenderFileIcon filename={file.name} />
       <span className="cursor-pointer flex justify-center items-center w-fit mr-2 p-1 rounded-md">
         {file.name}
       </span>
-      <span className="cursor-pointer hover:bg-[#64646473] duration-300 flex justify-center items-center w-fit mr-1 rounded-md">
+      <span className="cursor-pointer hover:bg-[#64646473] duration-300 flex justify-center items-center w-fit mr-1 rounded-md"
+        onClick={(e) => {
+          e.stopPropagation();
+          onRmove(file.id)
+        }}
+      >
         <CloseIcon />
       </span>
     </div>
